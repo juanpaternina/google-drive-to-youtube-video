1075017b03d0fa86847b2e40d26531703c48ab26
Juan Paternina <juan.paternina@gmail.com>
2022-12-07 11:47:04 -0500

    Update Google.

--- google.py
+++ google.py
@@ -1,3 +1,4 @@
+import datetime
 import pickle
 import os
 from google_auth_oauthlib.flow import Flow, InstalledAppFlow
@@ -7,17 +8,17 @@ from google.auth.transport.requests import Request
 
 
 def Create_Service(client_secret_file, api_name, api_version, *scopes):
-    print(client_secret_file, api_name, api_version, scopes, sep='-')
+    # print client_secret_file, api_name, api_version, scopes, sep='-'
     CLIENT_SECRET_FILE = client_secret_file
     API_SERVICE_NAME = api_name
     API_VERSION = api_version
     SCOPES = [scope for scope in scopes[0]]
-    print(SCOPES)
+    # print(SCOPES)
 
     cred = None
 
-    pickle_file = f'token_{API_SERVICE_NAME}_{API_VERSION}.pickle'
-    # print(pickle_file)
+    pickle_file = "token_{}_{}.pickle".format(API_SERVICE_NAME, API_VERSION)
+    print(pickle_file)
 
     if os.path.exists(pickle_file):
         with open(pickle_file, 'rb') as token:
